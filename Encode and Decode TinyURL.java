783b5937497d141529fea362b3f517d388902d20
Hemlatha Pandey <pandey.hema98@gmail.com>
2021-03-16 00:24:36 +0530

    Update Encode and Decode TinyURL.java

--- Encode and Decode TinyURL.java
+++ Encode and Decode TinyURL.java
@@ -1,1 +1,21 @@
+public class Codec {
 
+    // Encodes a URL to a shortened URL.
+    Map<String,String> m = new HashMap<>();
+    
+    
+    public String encode(String longUrl) {
+    String s = "http://tinyurl.com"+m.size();
+        m.put(s,longUrl);
+        return s;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return m.get(shortUrl);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));

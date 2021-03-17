7538ea786e1e2b5d17b9f919a2999e5780bf70ae
Hemlatha Pandey <pandey.hema98@gmail.com>
2021-03-17 23:44:33 +0530

    Create  Generate Random Point in a Circle.java

---
+++ Generate Random Point in a Circle.java
@@ -0,0 +1,6 @@
+/* properties of circle
+# any point with X and Y coordinates and a,b as the center of the circle
+-> (X-a)^2 + (Y-b)^2 = R*R (then X,Y on the circle)
+-> (X-a)^2 + (Y-b)^2 > R*R (then X,Y outside the circle)
+-> (X-a)^2 + (Y-b)^2 < R*R (then X,Y inside the circle)
+*/
